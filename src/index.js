b91027a8a9367263ab604414240873301023b936
Ryan Hanley <camelCaseGuy@users.noreply.github.com>
2023-11-26 01:13:26 -0600

    finished up look, login

--- src/index.js
+++ src/index.js
@@ -9,26 +9,36 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-} 
+}
 
 const output = document.getElementById('output');
-// const 
+let searchParams = new URLSearchParams(window.location.search);
+output.innerHTML = searchParams.get('username') ? `Welcome, <br />${searchParams.get("username")}!!` : 'Welcome!!';
+const username = document.getElementById('username');
+
 
 document.getElementById('btnLogin').addEventListener('click', () => {
+  if ('URLSearchParams' in window) {
+    let searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("username", username.value);
+    window.location.search = searchParams.toString();
+  }
+
   modal.style.display = "none";
-  output.value = username.value;
+  output.value = 'Hello ' + username.value + '!';
+  output.value = searchParams.get("username");
 })
